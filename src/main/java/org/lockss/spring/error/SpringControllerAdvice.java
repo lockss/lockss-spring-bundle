2bbb206579eba09b9d15a47e2450188b1a005032
Daniel Vargas <dlvargas@stanford.edu>
2021-03-25 23:17:25 -0700

    Removed @ExceptionHandlers for exceptions that are handled by DefaultHandlerExceptionResolver

--- src/main/java/org/lockss/spring/error/SpringControllerAdvice.java
+++ src/main/java/org/lockss/spring/error/SpringControllerAdvice.java
@@ -30,106 +30,15 @@
 
 package org.lockss.spring.error;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.lockss.log.L4JLogger;
+import org.lockss.util.rest.RestResponseErrorBody;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.HttpMediaTypeNotAcceptableException;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping(produces = "application/vnd.error+json")
 public class SpringControllerAdvice {
-
-  private static Logger log =
-      LoggerFactory.getLogger(SpringControllerAdvice.class);
-
-  /**
-   * Handles the exception thrown when the request handler cannot generate a response that is acceptable by the client.
-   */
-  @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
-  public ResponseEntity<RestResponseErrorBody> handler(final HttpMediaTypeNotAcceptableException e) {
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-
-    return new ResponseEntity<>(new RestResponseErrorBody(e.getMessage(), e.getClass().getSimpleName()), headers,
-        HttpStatus.NOT_ACCEPTABLE);
-  }
-
-  /**
-   * Handles the exception thrown when a client POSTs, PUTs, or PATCHes content of a type not supported by request
-   * handler.
-   */
-  @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-  public ResponseEntity<RestResponseErrorBody> handler(final HttpMediaTypeNotSupportedException e) {
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-
-    return new ResponseEntity<>(new RestResponseErrorBody(e.getMessage(), e.getClass().getSimpleName()), headers,
-        HttpStatus.UNSUPPORTED_MEDIA_TYPE);
-  }
-
-  /**
-   * Handles the exception thrown when an endpoint does not support the request HTTP method.
-   */
-  @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-  public ResponseEntity<RestResponseErrorBody> handler(final HttpRequestMethodNotSupportedException e) {
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-
-    return new ResponseEntity<>(new RestResponseErrorBody(e.getMessage(), e.getClass().getSimpleName()), headers,
-        HttpStatus.METHOD_NOT_ALLOWED);
-  }
-
-  /**
-   * Handles the exception thrown when there was an error parsing a HTTP request by a {@link HttpMessageConverter}.
-   */
-  @ExceptionHandler(HttpMessageNotReadableException.class)
-  public ResponseEntity<RestResponseErrorBody> handler(final HttpMessageNotReadableException e) {
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-
-    return new ResponseEntity<>(new RestResponseErrorBody(e.getMessage(), e.getClass().getSimpleName()), headers,
-        HttpStatus.BAD_REQUEST);
-  }
-
-  /**
-   * Handles the exception thrown when a request parameter has failed validation (e.g., malformed).
-   */
-  @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<RestResponseErrorBody> handler(final MethodArgumentNotValidException e) {
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-
-    return new ResponseEntity<>(new RestResponseErrorBody(e.getMessage(), e.getClass().getSimpleName()), headers,
-        HttpStatus.BAD_REQUEST);
-  }
-
-  /**
-   * Handles the exception thrown when a required request parameter is missing from the request.
-   */
-  @ExceptionHandler(MissingServletRequestParameterException.class)
-  public ResponseEntity<RestResponseErrorBody> handler(final MissingServletRequestParameterException e) {
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-
-    return new ResponseEntity<>(new RestResponseErrorBody(e.getMessage(), e.getClass().getSimpleName()), headers,
-        HttpStatus.BAD_REQUEST);
-  }
+  private static L4JLogger log = L4JLogger.getLogger();
 
   /**
    * Handles a custom LOCKSS REST service exception.
@@ -139,28 +48,7 @@ public class SpringControllerAdvice {
    * JSON format with media type {@code application/vnd.error+json}.
    */
   @ExceptionHandler(LockssRestServiceException.class)
-  public ResponseEntity<RestResponseErrorBody> handler(
-      final LockssRestServiceException lrse) {
-
-    HttpHeaders responseHeaders = new HttpHeaders();
-    responseHeaders.setContentType(MediaType.APPLICATION_JSON);
-
-    return new ResponseEntity<>(new RestResponseErrorBody(lrse), responseHeaders,
-        lrse.getHttpStatus());
-  }
-
-  /**
-   * Handles any other unhandled exception as a last resort.
-   *
-   * @param e An Exception with the exception not handled by other exception
-   * handler methods.
-   * @return a ResponseEntity<RestResponseErrorBody> with the error response in
-   * JSON format with media type {@code application/vnd.error+json}.
-   */
-  @ExceptionHandler(Exception.class)
-  public ResponseEntity<RestResponseErrorBody> defaultHandler(Exception e) {
-    log.error("Caught otherwise unhandled exception", e);
-    return new ResponseEntity<>(new RestResponseErrorBody(e.getMessage(),
-        e.getClass().getSimpleName()), HttpStatus.INTERNAL_SERVER_ERROR);
+  public ResponseEntity<RestResponseErrorBody.RestResponseError> handler(final LockssRestServiceException lrse) {
+    return new ResponseEntity<>(lrse.toRestResponseError(), lrse.getHttpStatus());
   }
 }
